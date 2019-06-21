860cd344b876fd0e4891706ad4dcda4ca7eeafe1
Jack Atherton <jackrrye@gmail.com>
2019-06-21 00:06:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,14 @@ def my_each(array)
   counter = 0 
   while counter < array.size do 
     #do something to the element in our array
-    yield()
+    yield(array[counter])
     #continue counter
     
     #return our array
 end
 
 collection = [1, 2, 3, 4]
+
 my_each(collection) do |i|
   puts i
 end
